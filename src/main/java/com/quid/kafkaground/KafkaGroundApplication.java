5c5e9c86f68aa1a4e0e73c9e9747a1450454a4db
QuiD-0 <wodnd101@naver.com>
2023-01-20 09:16:43 +0900

    init proj

---
+++ src/main/java/com/quid/kafkaground/KafkaGroundApplication.java
@@ -0,0 +1,13 @@
+package com.quid.kafkaground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quid/kafkaground/KafkaGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quid.kafkaground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
